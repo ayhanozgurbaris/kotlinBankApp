fe1afb91761675f66fa6554ecb8fd0bb4a7fa7c8
Özgür Barış Ayhan <barisayhan01@gmail.com>
2022-10-09 19:03:26 +0300

    19:03

---
+++ app/src/main/java/com/example/onucsifiruc/EmptyScreen.kt
@@ -0,0 +1,11 @@
+package com.example.onucsifiruc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmptyScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_empty_screen)
+    }
+}
