039c3e18bd563d054ad3c0efafd0f7b250dad693
Özgür Barış Ayhan <barisayhan01@gmail.com>
2022-10-10 14:00:20 +0300

    14:00

--- app/src/main/java/com/example/onucsifiruc/MainActivity.kt
+++ app/src/main/java/com/example/onucsifiruc/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.onucsifiruc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginKey = findViewById<Button>(R.id.loginId);
+        loginKey.setOnClickListener{
+
+            val Intent = Intent(this,EmptyScreen::class.java)
+            startActivity(Intent)
+        }
     }
 }
