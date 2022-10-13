cc28488aa96deaac87dd4e479006b1ffd0f6a987
Özgür Barış Ayhan <64589883+ayhanozgurbaris@users.noreply.github.com>
2022-10-13 18:15:28 +0300

    bannerButton

--- app/src/main/java/com/example/onucsifiruc/MainActivity.kt
+++ app/src/main/java/com/example/onucsifiruc/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.cardview.widget.CardView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -26,6 +27,15 @@ class MainActivity : AppCompatActivity() {
             callEmptyActivity()
         }
 
+        val bannerOneKey = findViewById<CardView>(R.id.bannerone);
+        bannerOneKey.setOnClickListener{
+            callEmptyActivity()
+        }
+
+        val bannerTwoKey = findViewById<CardView>(R.id.bannertwo);
+        bannerTwoKey.setOnClickListener{
+            callEmptyActivity()
+        }
 
 
         val hesabinlaNelerYapabilirsinKey = findViewById<Button>(R.id.hesabınlaNelerYapabilirsin);
